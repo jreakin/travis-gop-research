a0a1ddbe27ff8adfd4046a589ce07256792f1a41
jreakin <johnreakin@gmail.com>
2025-02-28 21:46:58 -0600

    Additional data files

--- __init__.py
+++ __init__.py
@@ -3,6 +3,19 @@ import pandas as pd
 
 CONFIG = tomli.load(open("config.toml", 'rb'))
 
+def change_non_ints_to_string(func):
+
+    def wrapper(*args, **kwargs):
+        df = func(*args, **kwargs)
+        for col in df.columns:
+            if col in ['expendAmount', 'Payment_Amount', 'contribution_receipt_amount', 'Total']:
+                df[col] = df[col].astype(float).round(2)
+            elif df[col].dtype == 'float64' or df[col].dtype == 'int64':
+                df[col] = df[col].astype(str)
+        return df
+    return wrapper
+
+@change_non_ints_to_string
 def set_datetime(df: pd.DataFrame, col: str = 'expendDt', fmt: str = '%Y%m%d') -> pd.DataFrame:
     df[col] = pd.to_datetime(df[col], format=fmt)
     return df
